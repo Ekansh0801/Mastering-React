44f9447c2ef0379a43b1d62a4cc08315f8cce697
Ekansh0801 <135941717+Ekansh0801@users.noreply.github.com>
2024-10-07 12:30:23 +0530

    Learnt Parcel ,JSX

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,15 @@
+const div = React.createElement("div", {
+    id: "container"
+}, [
+    React.createElement("h1", {}, "I am Heading h1"),
+    React.createElement("h2", {}, "I am Heading h2")
+]);
+// console.log(div)  // this is a js object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(div); // Bundlers
+ // webpack --> npx create-react-app  
+ // vite --> npx create @vite/latest
+ // Parel --> 
+ // npm --> 
+
+//# sourceMappingURL=index.7c0ccee6.js.map
